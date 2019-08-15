6c2299ac70d8cd1c6b8dc87a1eb3077e1a10e22f
Johan Gouws <u16033915@tuks.co.za>
2019-08-15 22:14:42 +0200

    Final Demo

--- Simulator.py
+++ Simulator.py
@@ -9,17 +9,18 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-test =        False
+test =        True
 doTrace =     True
-doQueuelen =  False
+doQueuelen =  True
 doLambdMus  = True
-genTrace =    False
+genTrace =    True
+
 # Read the text file and the inter-arrival time and the size
 def readCSV(filePath):
     arrival = []
     size = []
     
-    with open(filePath,'r')as f:
+    with open(filePath,'r') as f:
         data = csv.reader(f)
         for row in data:
             arrival.append(row[0])
@@ -43,6 +44,7 @@ def calculateParameters(interArrival, packetSize, transCap):
     idleTimes = []
     respTimes = []
     currTime = 0
+    
     
     # Calculate when the packets were received
     for i in range(0, len(interArrival)):
@@ -141,7 +143,7 @@ def queuLenVsTime(packetSz, linkCap):
     total = []
     time = []
 
-    for p in range(0,100):
+    for p in range(0,200):
         print(p)
         allQueues = []
         
@@ -187,7 +189,7 @@ def queuLenVsTime(packetSz, linkCap):
         
     plt.figure()
     for i in range(0, len(total)):
-        plt.plot(time[i], total[i], label=str(workloads[i]) + 'packets sent')
+        plt.plot(time[i], total[i], label=str(simPerc[i]*100) + ' % load')
     plt.legend(loc='best')
     plt.xlabel('Time (microseconds)')
     plt.ylabel('Customers waiting in queue')
@@ -196,7 +198,7 @@ def queuLenVsTime(packetSz, linkCap):
     
 def calcLambMuWorkloads(packetSz, linkCap):
     serviceRate = linkCap/packetSz
-    simPerc = [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+    simPerc = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     arrRate = []
     avgInterArr = []
     
@@ -204,7 +206,6 @@ def calcLambMuWorkloads(packetSz, linkCap):
         arrRate.append(i*serviceRate)
         avgInterArr.append(10**6/arrRate[-1])
       
-
     workloads = []
     for i in arrRate:
         workloads.append(i*100)
@@ -213,7 +214,7 @@ def calcLambMuWorkloads(packetSz, linkCap):
     mus = [0] * len(workloads)
     queueDels = [0] * len(workloads)
 
-    for p in range(0,100):
+    for p in range(0,10):
         print(p)
         
         for z in range(0, len(workloads)):
@@ -222,22 +223,31 @@ def calcLambMuWorkloads(packetSz, linkCap):
             # Read the csv file data into respective lists to store the data
             interArrival = np.random.exponential(int(avgInterArr[z]), int(workloads[z]))
             packetSize = np.random.exponential(int(packetSz), int(workloads[z]))
-                
+   
             # Get the parameters
             arrivalTimes,transTimes,queuingDelays,startEnd,idleTimes,respTimes = calculateParameters(interArrival, packetSize, transCap)
+            
             sizeAvg = sum(packetSize) / float(len(packetSize))
-           
-            lambdas[z] += arrivalTimes[-1]/len(packetSize)
+            lambdas[z] += (len(packetSize))/(sum(interArrival)/10**6)
             mus[z] += transCap/sizeAvg
-            queueDels[z] = sum(queuingDelays)/len(queuingDelays)
+            queueDels[z] += sum(queuingDelays)/len(queuingDelays)
+      
+    lambdas = np.asarray(lambdas)/10
+    mus = np.asarray(mus)/10
+    queueDels = np.asarray(queueDels)/10
     
-    lambdas = np.asarray(lambdas)/100
-    mus = np.asarray(mus)/100
-    queueDels = np.asarray(queueDels)/100
+    print("Lambdas            at [10%. 20%, ...., 80%, 90%] workload =")
+    for i in lambdas:
+          print(i)
     
-    print("Lambdas            at [10%. 20%, ...., 80%, 90%] workload =", lambdas)
-    print("Mu's               at [10%. 20%, ...., 80%, 90%] workload =", mus)
-    print("Avg queuing delays at [10%. 20%, ...., 80%, 90%] workload =", queueDels)
+    print("Mu's               at [10%. 20%, ...., 80%, 90%] workload =")
+    for i in mus:
+          print(i)
+    
+    print("Avg queuing delays at [10%. 20%, ...., 80%, 90%] workload =")
+    for i in queueDels:
+          print(i)
+    
 
 
 # List of inter-arrival times of the packages received
@@ -300,6 +310,8 @@ if (doTrace == True):
         arrivalTimes,transTimes,queuingDelays,startEnd,idleTimes,respTimes = calculateParameters(interArrival, packetSize, transCap)
         sizeAvg = sum(packetSize) / float(len(packetSize))
             
+        
+  
         if (transCap == 1*10**6):
             print("Link capacity --------------------", transCap, "bps") 
             print("Number of packets ---------------", len(packetSize), "packets" )
@@ -308,11 +320,11 @@ if (doTrace == True):
             print("Average response time -----------", sum(respTimes)/len(respTimes), "us" )
             print("The average packet size is ------", sizeAvg, "bits" )
             print("Average delays ------------------", sum(queuingDelays)/len(queuingDelays), "us")
-            print("Average arrival rate (lambda)----", arrivalTimes[-1]/len(packetSize) )
+            print("Average arrival rate (lambda)----", (len(packetSize))/(sum(interArrival)/10**6))
             print("Average service rate (mu) -------", transCap/sizeAvg)
             
         
-        time.append(np.arange(0, startEnd[-1][1], 100000))
+        time.append(np.arange(0, startEnd[-1][1], 1000000))
         queue = []
         
         for i in range(0, len(time[z])):
@@ -336,10 +348,10 @@ if (doTrace == True):
     plt.show()
 
 if (doQueuelen == True):
-    queuLenVsTime(1000, 100*10**6)
+    queuLenVsTime(1000, 100*10**2)
     
 if (doLambdMus == True):
-    calcLambMuWorkloads(1000, 10*10**6)
+    calcLambMuWorkloads(1000, 1*10**6)
 
 if (genTrace == True):
     generateTrace(0.5, 100*10**6, 1000)
